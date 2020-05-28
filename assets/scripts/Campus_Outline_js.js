cffa4402bbe83140737599a532eb45b29529f734
Daniel Teddison <danieteddison5@gmail.com>
2020-05-28 05:27:18 -0600

    just open and c it uh will find it out

---
+++ assets/scripts/Campus_Outline_js.js
@@ -0,0 +1,97 @@
+function openNav001() {
+  document.getElementById("myNav001").style.width = "100%";
+}
+
+function closeNav001() {
+  document.getElementById("myNav001").style.width = "0%";
+}
+
+function openNav002() {
+  document.getElementById("myNav002").style.width = "100%";
+}
+
+function closeNav002() {
+  document.getElementById("myNav002").style.width = "0%";
+}
+function openNav003() {
+  document.getElementById("myNav003").style.width = "100%";
+}
+
+function closeNav003() {
+  document.getElementById("myNav003").style.width = "0%";
+}
+function openNav004() {
+  document.getElementById("myNav004").style.width = "100%";
+}
+
+function closeNav004() {
+  document.getElementById("myNav004").style.width = "0%";
+}
+
+function openNav005() {
+  document.getElementById("myNav005").style.width = "100%";
+}
+
+function closeNav005() {
+  document.getElementById("myNav005").style.width = "0%";
+}
+function openNav006() {
+  document.getElementById("myNav006").style.width = "100%";
+}
+
+function closeNav006() {
+  document.getElementById("myNav006").style.width = "0%";
+}
+function openNav007() {
+  document.getElementById("myNav007").style.width = "100%";
+}
+
+function closeNav007() {
+  document.getElementById("myNav007").style.width = "0%";
+}
+
+function openNav008() {
+  document.getElementById("myNav008").style.width = "100%";
+}
+
+function closeNav008() {
+  document.getElementById("myNav008").style.width = "0%";
+}
+
+function openNav009() {
+  document.getElementById("myNav009").style.width = "100%";
+}
+
+function closeNav009() {
+  document.getElementById("myNav009").style.width = "0%";
+}
+
+function openNav010() {
+  document.getElementById("myNav010").style.width = "100%";
+}
+
+function closeNav010() {
+  document.getElementById("myNav010").style.width = "0%";
+}
+function openNav011() {
+  document.getElementById("myNav011").style.width = "100%";
+}
+
+function closeNav011() {
+  document.getElementById("myNav011").style.width = "0%";
+}
+function openNavLD() {
+  document.getElementById("myNavLD").style.width = "100%";
+}
+
+function closeNavLD() {
+  document.getElementById("myNavLD").style.width = "0%";
+}
+
+function openNavSTEPS() {
+  document.getElementById("myNavSTEPS").style.width = "100%";
+}
+
+function closeNavSTEPS() {
+  document.getElementById("myNavSTEPS").style.width = "0%";
+}
